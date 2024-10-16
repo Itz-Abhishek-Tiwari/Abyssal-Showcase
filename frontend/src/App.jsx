7d842e315565d5ef0e5f344efb18789e6bd3d916
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-10-16 14:57:22 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home"
+
+function App() {
+
+  return (
+    <>
+      <body className="h-screen bg-zinc-100">
+        <Home />
+      </body>
+    </>
+  )
+}
+
+export default App
